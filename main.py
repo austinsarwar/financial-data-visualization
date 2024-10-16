60ec7ffa8393b9ded36a9b6a35309791c2106056
austinsarwar <austin_sarwar@yahoo.com>
2024-10-16 02:45:30 -0400

    Change graph type

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ def visualize_data(main_directory : str, target_directory: str,  target_file: st
     
             
     for file in os.listdir(path_to_target):
-            if(target_file in file.lower()):
+            if(target_file.lower() in file.lower()):
                 path_to_return = os.path.join(path_to_target, file)
                 df = pd.read_csv(path_to_return)
 
-
+    # Remove null values from Data Frame
+    df = df.dropna()
+    df_count = len(df)
     df['Date'] = pd.to_datetime(df['Date'])
     
     print(f"Info: {df.info()}")
@@ -32,24 +34,35 @@ def visualize_data(main_directory : str, target_directory: str,  target_file: st
     fig, axs = plt.subplots(2,2, figsize=(12, 10))
 
     axs1 = axs[0][0]
-    axs1 .set_title("Close Price History")
-    axs1 .plot(df.loc[:, 'Date'], df['Close'])
+    axs1.set_title("Price History last 20 Years")
+    axs1.plot(df.loc[:, 'Date'], df.loc[:, 'Close'])
 
     axs2 = axs[0][1]
-    axs2.set_title("Open Price vs Volume")
-    axs2.scatter(df['Volume'], df['Close'], color='orange', alpha=0.5, marker='x')
-    axs2.set_xlabel('Volume')
-    axs2.set_ylabel('Close Price')
+    axs2.set_title("Price history over Volume 2023")
+    axs2.bar(df.loc[df_count-365:5145, 'Date'], df.loc[df_count-365:5145, 'Volume'])
+    axs2.set_yscale('log')
+
+    axs2_sub = axs2.twinx()  # instantiate a second Axes that shares the same x-axis
+    color = 'tab:green'
+    axs2_sub.set_ylabel('Price', color=color)  # we already handled the x-label with ax1
+    axs2_sub.plot(df.loc[df_count-365:5145, 'Date'], df.loc[df_count-365:5145, 'Close'], color='green')
+    axs2_sub.tick_params(axis='y', labelcolor=color)
 
     axs3 = axs[1][0]
-    axs3.set_title("Volume")
+    axs3.set_title("Price history over Volume last 20 years")
     axs3.bar(df['Date'], df['Volume'])
     axs3.set_yscale('log')
 
+    axs3_sub = axs3.twinx()  # instantiate a second Axes that shares the same x-axis
+    color = 'tab:blue'
+    axs3_sub.set_ylabel('Price', color=color)  # we already handled the x-label with ax1
+    axs3_sub.plot(df.loc[:, 'Date'], df['Close'])
+    axs3_sub.tick_params(axis='y', labelcolor=color)
 
     axs4 = axs[1][1]
-    axs4.set_title("Open price history")
-    axs4.plot(df['Date'], df['YTD Gains'])
+    axs4.set_title("Price history 2023")
+    axs4.plot(df.loc[df_count-365:5145, 'Date'], df.loc[df_count-365:5145, 'Close'])
+
     
     for i in range(2):
         for j in range(2):    
@@ -60,7 +73,7 @@ def visualize_data(main_directory : str, target_directory: str,  target_file: st
 
     plt.show()
     
-visualize_data(path, "stocks", "aapl")
+visualize_data(path, "stocks", "pep")
 
 
 
