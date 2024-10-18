d66987257473ba7e387b8233c574eacd4400b247
austinsarwar <austin_sarwar@yahoo.com>
2024-10-17 21:24:14 -0400

    add moving averages, refine charts

--- main.py
+++ main.py
@@ -23,10 +23,17 @@ def visualize_data(main_directory : str, target_directory: str,  target_file: st
                 path_to_return = os.path.join(path_to_target, file)
                 df = pd.read_csv(path_to_return)
 
-    # Remove null values from Data Frame
     df = df.dropna()
-    df_count = len(df)
+   
     df['Date'] = pd.to_datetime(df['Date'])
+    df["1000_day_average"] = df["Close"].rolling(1000).mean()
+    df["200_day_average"] = df["Close"].rolling(250).mean()
+    df["50_day_average"] = df["Close"].rolling(50).mean()
+
+    # Clean data remove null values
+   
+    # Get the length of the dataframe
+    df_count = len(df)
     
     print(f"Info: {df.info()}")
     print(df)
@@ -36,22 +43,22 @@ def visualize_data(main_directory : str, target_directory: str,  target_file: st
     axs1 = axs[0][0]
     axs1.set_title("Price History last 20 Years")
     axs1.plot(df.loc[:, 'Date'], df.loc[:, 'Close'])
+   
+    # axs1_sub = axs1.twinx()
+    # axs1_sub.plot(df.loc[:, 'Date'], df.loc[:, '200_day_average'])
 
     axs2 = axs[0][1]
-    axs2.set_title("Price history over Volume 2023")
-    axs2.bar(df.loc[df_count-365:5145, 'Date'], df.loc[df_count-365:5145, 'Volume'])
-    axs2.set_yscale('log')
-
-    axs2_sub = axs2.twinx()  # instantiate a second Axes that shares the same x-axis
-    color = 'tab:green'
-    axs2_sub.set_ylabel('Price', color=color)  # we already handled the x-label with ax1
-    axs2_sub.plot(df.loc[df_count-365:5145, 'Date'], df.loc[df_count-365:5145, 'Close'], color='green')
-    axs2_sub.tick_params(axis='y', labelcolor=color)
+    axs2.set_title("Price history 20 years with moving averages")
+    axs2.plot(df.loc[:, 'Date'], df.loc[:, 'Close'], label='Stock Price')
+    axs2.plot(df.loc[:, 'Date'], df.loc[:, '200_day_average'], color='green', label='200 day avg')
+    axs2.plot(df.loc[:, 'Date'], df.loc[:, '50_day_average'], color='orange', label='50 day avg')
+    axs2.legend(loc='upper left')
 
     axs3 = axs[1][0]
     axs3.set_title("Price history over Volume last 20 years")
     axs3.bar(df['Date'], df['Volume'])
     axs3.set_yscale('log')
+
 
     axs3_sub = axs3.twinx()  # instantiate a second Axes that shares the same x-axis
     color = 'tab:blue'
@@ -60,20 +67,23 @@ def visualize_data(main_directory : str, target_directory: str,  target_file: st
     axs3_sub.tick_params(axis='y', labelcolor=color)
 
     axs4 = axs[1][1]
-    axs4.set_title("Price history 2023")
-    axs4.plot(df.loc[df_count-365:5145, 'Date'], df.loc[df_count-365:5145, 'Close'])
+    axs4.set_title("Price history 2022-2023")
+    dates = df.iloc[-366:, df.columns.get_loc('Date')].tolist()
+    close_values = df.iloc[-365:, df.columns.get_loc('Close')].tolist()
 
+    # Plot with correct axis ordering
+    axs4.stairs(close_values, dates)
     
     for i in range(2):
         for j in range(2):    
-            axs[i][j].xaxis.set_major_locator(plt.MaxNLocator(6))
+            axs[i][j].xaxis.set_major_locator(plt.MaxNLocator(10))
             axs[i][j].tick_params(axis='both', which='major', labelsize=10)
 
     plt.suptitle(f"Stock Price History for {target_file} ", fontsize=50)
 
     plt.show()
     
-visualize_data(path, "stocks", "pep")
+visualize_data(path, "stocks", "nvda")
 
 
 
